d6248bfbbdf9ec16515893caeaad0c5a421ca035
baurine <2008.hbl@gmail.com>
2024-05-27 18:31:20 +0800

    build lib

--- packages/lib/apps/slow-query/vite.config.ts
+++ packages/lib/apps/slow-query/vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main.tsx'),
+      name: 'SlowQueryApp',
+      fileName: 'slow-query-app'
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        },
+      },
+    }
+  }
 })
